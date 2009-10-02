bbbedff906c8d77e8f0299150176aaf204da09af
Arun Persaud <arun@nubati.net>
2009-10-02 16:56:01 -0700

    updated version number to 4.4.1.pre
    should have done this straight after the last release, but forgot...

--- winboard/config.h
+++ winboard/config.h
@@ -46,13 +46,13 @@
 #define PACKAGE_NAME "WinBoard"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "WinBoard 4.4.0"
+#define PACKAGE_STRING "WinBoard 4.4.1 pre"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "winboard"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "4.4.0"
+#define PACKAGE_VERSION "4.4.1.pre"
 
 #define PTY_ITERATION
 
