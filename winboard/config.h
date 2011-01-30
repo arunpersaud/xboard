bcb41f2f49da65859f3692c87ded41eb75be20b8
Arun Persaud <arun@nubati.net>
2011-01-30 09:52:15 -0800

    release of version 4.5.0

--- winboard/config.h
+++ winboard/config.h
@@ -46,16 +46,16 @@
 #define PACKAGE_NAME "WinBoard"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "WinBoard master-20101211"
+#define PACKAGE_STRING "WinBoard 4.5.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "winboard"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "master-20101211"
+#define PACKAGE_VERSION "4.5.0"
 
 /* Define the Windows-specific FILE version info.  this *MUST* be four comma separated 16-bit integers */
-#define PACKAGE_FILEVERSION 4,4,4,2
+#define PACKAGE_FILEVERSION 4,5,0,0
 
 #define PTY_ITERATION
 
