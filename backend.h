d68c9b74cdc8f453af426a9fd9e1b3fd1766205e
H.G. Muller <h.g.muller@hccnet.nl>
2011-04-16 13:40:16 +0200

    Fix gamelist highlight
    Reopening the gamelist should highlight the current game.

--- backend.h
+++ backend.h
@@ -278,6 +278,7 @@ typedef struct _ListGame {
  
 extern ChessSquare gatingPiece;
 extern List gameList;
+extern int lastLoadGameNumber;
 void ClearGameInfo P((GameInfo *));
 int GameListBuild P((FILE *));
 void GameListInitGameInfo P((GameInfo *));
