779f4452481365234bef2f236b196be64b628c44
H.G. Muller <h.g.muller@hccnet.nl>
2011-07-09 13:13:49 +0200

    Let WB eval graph react ot single left-click
    Unlike in XBoard, the WinBoard eval graph awatys had required double-
    clicking to go the the corresponding move, while there really is no
    reason for this, as it is no text edit, and single-clicks have no function
    (like select).

--- winboard/wevalgraph.c
+++ winboard/wevalgraph.c
@@ -212,7 +212,7 @@ LRESULT CALLBACK EvalGraphProc( HWND hDlg, UINT message, WPARAM wParam, LPARAM l
         ReleaseDC( hDlg, hDC );
         break;
 
-    case WM_LBUTTONDBLCLK:
+    case WM_LBUTTONDOWN:
         if( wParam == 0 || wParam == MK_LBUTTON ) {
             int index = GetMoveIndexFromPoint( LOWORD(lParam), HIWORD(lParam) );
 
