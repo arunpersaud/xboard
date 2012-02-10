4bb787125b2e9c75e2e62b9f9c19bc45bac8ef55
Arun Persaud <arun@nubati.net>
2012-02-09 23:03:58 -0800

    new developer release

--- winboard/config.h
+++ winboard/config.h
@@ -46,16 +46,16 @@
 #define PACKAGE_NAME "WinBoard"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "WinBoard master-20110822"
+#define PACKAGE_STRING "WinBoard master-20120209"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "winboard"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "master-20110822"
+#define PACKAGE_VERSION "master-20120209"
 
 /* Define the Windows-specific FILE version info.  this *MUST* be four comma separated 16-bit integers */
-#define PACKAGE_FILEVERSION 4,5,1,5
+#define PACKAGE_FILEVERSION 4,6,0,0
 
 #define PTY_ITERATION
 
