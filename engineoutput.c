2d64eed6cbca4b6c81c31bbc9471fa6f08312cea
H.G. Muller <h.g.muller@hccnet.nl>
2012-02-23 09:29:17 +0100

    Fix translation of EngineOutputTitle WB

--- engineoutput.c
+++ engineoutput.c
@@ -53,7 +53,13 @@
 # define  _(s) gettext (s)
 # define N_(s) gettext_noop (s)
 #else
-# define  _(s) (s)
+# ifdef WIN32
+#  define  _(s) T_(s)
+#  undef  ngettext
+#  define  ngettext(s,p,n) T_(p)
+# else
+#  define  _(s) (s)
+# endif
 # define N_(s)  s
 #endif
 
