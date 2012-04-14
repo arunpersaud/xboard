a1d587908e4628a2f50edb4cd6af36cd7e5b4b50
H.G. Muller <h.g.muller@hccnet.nl>
2012-04-14 13:27:48 +0200

    Suppress testing for availability in bughouse drops
    Give up on testing legality of bughouse drop moves; this only causes problems,
    because the move list sent by the ICS is not accompanied by holdings updates.
    So XBoard parses it thinking the holdings are empty, making every drop move
    an ImpossibleMove. Bughouse can only be played on ICS, and we might as well
    trust the ICS that it will only pass us legal moves. A user should not be
    able to enter illegal drops; unavailable pieces cannot be grabbed from the
    holdings, and typing is diverted to the ICS directly (which would reject the
    illegal drop without XBoard even knowing about it).
      Also improve switching to bh/zh, based on drop move in initial board.

--- backend.c
+++ backend.c
@@ -2441,6 +2441,7 @@ VariantSwitch (Board board, VariantClass newVariant)
 	   board[i][j+newHoldingsWidth-gameInfo.holdingsWidth] =
 	     board[i][j];
      }
+     board[HOLDINGS_SET] = 0;
      gameInfo.boardWidth  = newWidth;
      gameInfo.boardHeight = newHeight;
      gameInfo.holdingsWidth = newHoldingsWidth;
@@ -4302,6 +4303,7 @@ ParseBoard12 (char *string)
     }
 
    if (gameInfo.boardHeight != ranks || gameInfo.boardWidth != files ||
+					move_str[1] == '@' && !gameInfo.holdingsWidth ||
 					weird && (int)gameInfo.variant < (int)VariantShogi) {
      /* [HGM] We seem to have switched variant unexpectedly
       * Try to guess new variant from board size
@@ -4311,7 +4313,8 @@ ParseBoard12 (char *string)
 	  if(ranks == 10 && files == 9) newVariant = VariantXiangqi; else
 	  if(ranks == 8 && files == 12) newVariant = VariantCourier; else
 	  if(ranks == 9 && files == 9)  newVariant = VariantShogi; else
-	  if(!weird) newVariant = VariantNormal;
+	  if(ranks == 10 && files == 10) newVariant = VariantGrand; else
+	  if(!weird) newVariant = move_str[1] == '@' ? VariantCrazyhouse : VariantNormal;
           VariantSwitch(boards[currentMove], newVariant); /* temp guess */
 	  /* Get a move list just to see the header, which
 	     will tell us whether this is really bug or zh */
