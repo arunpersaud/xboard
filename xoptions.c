6f995a82b5c6de08f1d60efc7d5ac0cd1c639899
H.G. Muller <h.g.muller@hccnet.nl>
2012-10-20 10:01:35 +0200

    Add listbox double-click callback
    This function was formerly performed by WheelProc.

--- xoptions.c
+++ xoptions.c
@@ -951,6 +951,18 @@ void BrowseGTK(GtkWidget *widget, gpointer gdata)
     dialog = NULL;
 }
 
+gboolean
+ListCallback (GtkWidget *widget, GdkEventButton *event, gpointer gdata)
+{
+    int n = (intptr_t) gdata & 0xFFFF;
+    int dlg = (intptr_t) gdata >> 16;
+    Option *opt = dialogOptions[dlg] + n;
+
+    if(event->type != GDK_2BUTTON_PRESS || event->button != 1) return FALSE;
+    ((ListBoxCallback*) opt->textValue)(n, SelectedListBoxItem(opt));
+    return TRUE;
+}
+
 static char *oneLiner  =
    "<Key>Return: redraw-display() \n \
     <Key>Tab: TabProc() \n ";
@@ -1343,6 +1355,9 @@ GenericPopUp (Option *option, char *title, DialogClass dlgNr, DialogClass parent
                 gtk_container_add(GTK_CONTAINER(sw), list);
                 gtk_widget_set_size_request(GTK_WIDGET(sw), option[i].max ? option[i].max : -1, option[i].value ? option[i].value : -1);
  
+                if(option[i].textValue) // generic callback for double-clicking listbox item
+                    g_signal_connect(list, "button-press-event", G_CALLBACK(ListCallback), (gpointer) (dlgNr<<16 | i) );
+
                 /* never has label, so let listbox occupy all columns */
                 Pack(hbox, table, sw, left, left+3, top);
             }
