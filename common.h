19a31cbb25faf8b09ca5fd1ab7bb405f3f823452
H.G. Muller <h.g.muller@hccnet.nl>
2013-09-16 22:27:50 +0200

    Repair XBoard from node-count patch
    u64 was #defined in backend.h, which unfortunately was almost alwats
    (Not sure why WinBoard did not suffer from this!) This, and related
    definitions have now been moved to common.h (which is always #included
    first).

--- common.h
+++ common.h
@@ -112,6 +112,46 @@ typedef char *String;
 
 /* End compatibility grunge */
 
+/* unsigned int 64 for engine nodes work and display */
+#ifdef WIN32
+       /* I don't know the name for this type of other compiler
+        * If it not work, just modify here
+        * This is for MS Visual Studio
+        */
+       #ifdef _MSC_VER
+               #define u64 unsigned __int64
+               #define s64 signed __int64
+               #define u64Display "%I64u"
+               #define s64Display "%I64d"
+               #define u64Const(c) (c ## UI64)
+               #define s64Const(c) (c ## I64)
+       #else
+               /* place holder
+                * or dummy types for other compiler
+                * [HGM] seems that -mno-cygwin comple needs %I64?
+                */
+               #define u64 unsigned long long
+               #define s64 signed long long
+               #ifdef USE_I64
+                  #define u64Display "%I64u"
+                  #define s64Display "%I64d"
+               #else
+                  #define u64Display "%llu"
+                  #define s64Display "%lld"
+               #endif
+               #define u64Const(c) (c ## ULL)
+               #define s64Const(c) (c ## LL)
+       #endif
+#else
+       /* GNU gcc */
+       #define u64 unsigned long long
+       #define s64 signed long long
+       #define u64Display "%llu"
+       #define s64Display "%lld"
+       #define u64Const(c) (c ## ull)
+       #define s64Const(c) (c ## ll)
+#endif
+
 #define PROTOVER                2       /* engine protocol version */
 
 // [HGM] license: Messages that engines must print to satisfy their license requirements for patented variants
