fb2862a0281389bd310da45d97b4ac643e0ca5a4
Arun Persaud <arun@nubati.net>
2014-09-29 21:00:42 -0700

    new version number for release 4.8.0

--- winboard/config.h
+++ winboard/config.h
@@ -46,17 +46,17 @@
 #define PACKAGE_NAME "WinBoard"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "WinBoard master-20140119"
+#define PACKAGE_STRING "WinBoard 4.8.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "winboard"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "master-20140119"
+#define PACKAGE_VERSION "4.8.0"
 
 /* Define the Windows-specific FILE version info.  this *MUST* be four comma separated 16-bit integers */
 /* remember to not start a number with 0 (octal), dates like 2014,0901 would lead to an error */
-#define PACKAGE_FILEVERSION 4,7,2014,119
+#define PACKAGE_FILEVERSION 4,8,2014,929
 
 #define PTY_ITERATION
 
