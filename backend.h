d3afa9ebe97b1d9e05d5a843cd1bfe99981f7b5e
H.G.Muller <hgm@hgm-xboard.(none)>
2016-03-21 23:50:46 +0100

    Silence warning WB
    No prototype for SendToICS was in a sufficiently shared header.

--- backend.h
+++ backend.h
@@ -428,6 +428,7 @@ char *SupportedVariant P((char *list, VariantClass v, int w, int h, int s, int p
 char *CollectPieceDescriptors P((void));
 void RefreshSettingsDialog P((ChessProgramState *cps, int val));
 void StartChessProgram P((ChessProgramState *cps));
+void SendToICS P((char *s));
 int PosFlags P((int n));
 
 
